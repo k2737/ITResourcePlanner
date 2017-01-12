30d5b6633211ff3c062f6e39efb8489a5e6f97f4
Adam Brown <AdamLBrown@gmail.com>
2017-01-12 12:49:22 +0000

    Fixed SQL to form data gets inserted into database correctly.

--- input.php
+++ input.php
@@ -4,20 +4,30 @@
 include 'dbInfo.php';
 
 
-if (isset($_POST['submit'])) {
-$printerId = $_POST["printerId"];
-$date = $_POST["date"];
-$printerType = $_POST["printerType"];
-$printCount = $_POST["printCount"];
+//if (isset($_POST['submit'])) {
+$printerId = $_POST['printerID'];
+$printerType = $_POST['printerType'];
+$date = $_POST['date'];
+$printCount = $_POST['printCount'];
 
-$sql = "INSERT INTO PrintCounts (DeviceId, DeviceType, Date, PageCount) VALUES (`$printerId`,`$printerType`,`$date`,`$printCount` );";
-
-$order = $conn->Query($sql);
-
-if ($order) {
-    echo '<br>Input data is successful';
+mysqli_query($conn, "INSERT INTO PrintCounts (DeviceID, DeviceType, Date, PageCount) VALUES ('$printerId','$printerType','$date','$printCount')");
+				
+	if(mysqli_affected_rows($conn) > 0){
+	echo "<p>Print Count added!</p>";
+	echo "<a href='index.php'>Go Back</a>";
 } else {
-    echo '<br>Input data is not valid';
+	echo "Employee NOT Added<br />";
+	echo mysqli_error ($conn);
 }
-}
+
+//$sql = "INSERT INTO PrintCounts (DeviceId, DeviceType, Date, PageCount) VALUES (`$printerId`,`$printerType`,`$date`,`$printCount`);";
+
+//$order = $conn->Query($sql);
+
+//if ($order) {
+  //  echo '<br>Input data is successful';
+//} else {
+  //  echo '<br>Input data is not valid';
+//}
+//}
 ?>
