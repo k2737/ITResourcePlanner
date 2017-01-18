57e35a47f531e99f0248bbac818f4531378d53a8
Adam Brown <AdamLBrown@gmail.com>
2017-01-18 03:29:16 +0000

    Fixed tab menu so content shows correctly.

--- index.php
+++ index.php
@@ -1,12 +1,102 @@
-:<?php
-//MySQL Database Connect
-include 'dbInfo.php';
+<html>
+<head>
+<title>IT ERP</title>
+</head>
+<body>
+<style>
+/* Style the list */
+ul.tab {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+}
 
-echo "<html>";
-echo "<head>";
-echo "<title>IT ERP</title>";
-echo "</head>";
-echo "<body>";
+/* Float the list items side by side */
+ul.tab li {float: left;}
+
+/* Style the links inside the list items */
+ul.tab li a {
+    display: inline-block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    transition: 0.3s;
+    font-size: 17px;
+}
+
+/* Change background color of links on hover */
+ul.tab li a:hover {background-color: #ddd;}
+
+/* Create an active/current tablink class */
+ul.tab li a:focus, .active {background-color: #ccc;}
+
+/* Style the tab content */
+.tabcontent {
+    display: none;
+    padding: 6px 12px;
+    border: 1px solid #ccc;
+    border-top: none;
+}
+</style>
+
+
+<script>
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script>
+
+<ul class="tab">
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Dashboard')">DASHBOARD</a></li>
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Printers')">PRINTERS</a></li>
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Contracts')">CONTRACTS</a></li>
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'SSL_Certificates')">SSL CERTIFICATES</a></li>
+</ul>
+
+<div id='Dashboard' class='tabcontent'>
+  <h3>London</h3>
+  <p>London is the capital city of England.</p>
+</div>
+
+<div id='Printers' class='tabcontent'>
+  <h3>Paris</h3>
+  <p>Paris is the capital of France.</p>
+</div>
+
+<div id='Contracts' class='tabcontent'>
+  <h3>Tokyo</h3>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
+
+<div id='SSL_Certificates' class='tabcontent'>
+  <h3>Tokyo</h3>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
+
+<?php
+
+include 'dbInfo.php';
 
 $sql = "select DeviceName, Date, PageCount from PrintCounts JOIN Devices ON PrintCounts.DeviceID=Devices.DeviceID WHERE PrintCounts.DeviceID='3';";
 $result = $conn->query($sql);
